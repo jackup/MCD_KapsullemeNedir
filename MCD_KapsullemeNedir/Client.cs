c8c686e4a0245e9edf8c0250fc910f2e5b8e9d1b
jackup <yk.58@hotmail.com>
2021-11-29 17:29:45 +0300

    Add project files.

---
+++ MCD_KapsullemeNedir/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KapsullemeNedir
+{
+    class Client
+    {
+        //Class => Field
+        int id;
+        public string name, surname;
+
+        //Class => Property
+        private string emailAddress;
+        public string EmailAddress 
+        { 
+            //get;//Database veyafarklı bir veri kaynağı class çağıran programcıya datayı gösterdiğimiz alan
+            //set;//Dış dünyadan alınan datanın içerisinde private olarak saklanan field içerisine değer atandığı kısım
+        
+            set
+            {
+                this.emailAddress = value;
+            }
+            get
+            {
+                return this.emailAddress;
+            }
+        
+        }
+    }
+}
---
+++ MCD_KapsullemeNedir/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KapsullemeNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client C1 = new Client();
+            // Field değer ataması yapılmasın ama değer okunabilsin.
+            // Field değer ataması yapılsın ama değer okunamasın.
+            // Field değer ataması yapılsın ama değerin ilk dört hanesi okunsun.
+            // Field değer ataması ve okuma işlemleri yapılsın ama bu iki işlemde bizim belirlediğimiz bir süreçten geçsin. Veri daha sonra gösterilsin.
+
+
+
+            //Genel olarak kapsülleme konusuna giriş yaptık.
+            C1.EmailAddress = "yk.58@hotmail.com";
+            Console.WriteLine(C1.EmailAddress);
+        }
+    }
+}
